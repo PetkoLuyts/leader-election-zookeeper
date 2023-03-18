849a80c19f8ad4e54484dc883efa6b9c26755015
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-18 17:01:00 +0200

    Refactor the project to separate the logic

--- src/main/java/cluster/management/LeaderElection.java
+++ src/main/java/cluster/management/LeaderElection.java
@@ -1,3 +1,5 @@
+package cluster.management;
+
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 
@@ -7,26 +9,19 @@ import java.util.List;
 
 public class LeaderElection implements Watcher {
 
-    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
-    private static final int SESSION_TIMEOUT = 3000;
     private static final String ELECTION_NAMESPACE = "/election";
-    private ZooKeeper zooKeeper;
+    private final ZooKeeper zooKeeper;
     private String currentZnodeName;
 
-    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-        LeaderElection leaderElection = new LeaderElection();
-
-        leaderElection.connectToZookeeper();
-        leaderElection.volunteerForLeadership();
-        leaderElection.reelectLeader();
-        leaderElection.run();
-        leaderElection.close();
-        System.out.println("Disconnected from Zookeeper, exiting application");
+    public LeaderElection(ZooKeeper zooKeeper) {
+        this.zooKeeper = zooKeeper;
     }
 
     public void volunteerForLeadership() throws InterruptedException, KeeperException {
         String znodePrefix = ELECTION_NAMESPACE + "/c_";
-        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        String znodeFullPath = zooKeeper.create(znodePrefix,
+                new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL);
 
         System.out.println("znode full path " + znodeFullPath);
         this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
@@ -56,32 +51,9 @@ public class LeaderElection implements Watcher {
         System.out.println("Watching znode " + predecessorZnodeName);
     }
 
-    public void connectToZookeeper() throws IOException {
-        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
-    }
-
-    public void run() throws InterruptedException {
-        synchronized (zooKeeper) {
-            zooKeeper.wait();
-        }
-    }
-
-    private void close() throws InterruptedException {
-        zooKeeper.close();
-    }
     @Override
     public void process(WatchedEvent event) {
         switch (event.getType()) {
-            case None:
-                if (event.getState() == Event.KeeperState.SyncConnected) {
-                    System.out.println("Successfully connected to Zookeeper");
-                } else {
-                    synchronized (zooKeeper) {
-                        System.out.println("Disconnected from Zookeeper event");
-                        zooKeeper.notifyAll();
-                    }
-                }
-                break;
             case NodeDeleted:
                 try {
                     reelectLeader();
