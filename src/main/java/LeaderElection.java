ed3971bc7f91b20558e4d31ec86453d01c952c8a
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-18 11:09:08 +0200

    Add log4j properties

---
+++ src/main/java/LeaderElection.java
@@ -0,0 +1,32 @@
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.server.quorum.Leader;
+
+import java.io.IOException;
+
+public class LeaderElection implements Watcher {
+
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws IOException {
+        LeaderElection leaderElection = new LeaderElection();
+        leaderElection.connectToZookeeper();
+    }
+
+    public void connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to Zookeeper");
+                }
+        }
+    }
+}
